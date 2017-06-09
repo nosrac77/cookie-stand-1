b6f3346f921c74ba8c0eb99cf35f712ecf4ceb63
krichey15 <richey.kyle.i@gmail.com>
2017-06-08 22:12:01 -0700

    added sales and created objects

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var pike = {
+  custMin: 23,
+  custMax: 65,
+  avgCookie: 6.3
+};
+
+var seaTac = {
+  custMin: 3,
+  custMax: 24,
+  avgCookie: 1.2
+}
+
+var seattleCenter = {
+  custMin: 11,
+  custMax: 38,
+  avgCookie: 3.7
+};
+
+var capitolHill = {
+  custMin: 20,
+  custMax: 38,
+  avgCookie: 2.3
+};
+
+var alki = {
+  custMin: 2,
+  custMax: 16,
+  avgCookie: 4.6
+};
