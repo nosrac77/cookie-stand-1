7aab47ff63bde67896edbd81bd3bc2b451fcc90d
krichey15 <richey.kyle.i@gmail.com>
2017-06-12 13:32:56 -0700

    cleaned up code so it flows better

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 'use strict';
 
+
+
+/*I want to create a function that will push an array of hoursOpen into an object constructor so i dont have to do it for each store. */
+
+/*I want to take randomCount and multiply it by avgCookie 15 times (for 15 hours) and push the product into the cookiePerHour array within each store object.*/
+
+var tableEl = document.createElement('table');
+document.body.appendChild(tableEl);
+
 function randomCust(min, max){
   var randomCount = Math.floor(Math.random() * (max + 1 - min)) + min;
   // console.log(randomCount);
   return randomCount;
 }
 
-/*I want to create a function that will push an array of hoursOpen into an object constructor so i dont have to do it for each store. */
 function pushHoursOpen(store){
   for(var i = 1; i < storeObjects.length; i++){
   store.hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
   }
 }
 
-/*I want to take randomCount and multiply it by avgCookie 15 times (for 15 hours) and push the product into the cookiePerHour array within each store object.*/
 function perHour(store){
   for(var i = 1; i <= store.hoursOpen.length; i++){
     var cookies = store.cookiePerHour.push(randomCust(store.custMin, store.custMax) * Math.floor(store.avgCookie));
@@ -21,9 +28,6 @@ function perHour(store){
   };
   console.log(store.cookiePerHour);
 }
-
-var tableEl = document.createElement('table');
-document.body.appendChild(tableEl);
 
 function putInHours(){
   var hours = [' ', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
@@ -37,10 +41,6 @@ function putInHours(){
     tableDataEl.textContent = hours[i];
   };
 }
-
-// var tableRowEl = document.createElement('tr');
-// tableEl.appendChild(tableRowEl);
-// tableDataEl.textContent = //I want to put the hoursOpen here...figure out how.
 
 /*I want change the code below so that it creates a table instead of a list. */
 function pushToSales(store){
